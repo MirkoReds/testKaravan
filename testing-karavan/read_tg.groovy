292b3c7cfaa1b83974af68c960499e187b37f4a0
developer <developer@platform.platform>
2026-01-29 15:14:20 +0000

    29/01/2026, 16:14:18

--- testing-karavan/read_tg.groovy
+++ testing-karavan/read_tg.groovy
@@ -1,13 +1,13 @@
 import java.util.HashMap;
-//import groovy.json.JsonOutput;
-import com.fasterxml.jackson.databind.ObjectMapper
 
 def test = body
 List<String> arrText = new ArrayList<>()
+def outputString = "La lista dei messaggi è: "
 
 test.result?.each { item -> 
     arrText.add(item.message?.text as String)
     println item.message?.text
+    outputString = outputString + (item.message?.text as String) + ", "
 }
 
-body = arrText
+body = outputString
