75bd83b0e74be78218bb3c46984c38fd05d82ec5
Mirko Rossi <rossimirko97@gmail.com>
2026-01-29 16:39:42 +0100

    Update read_tg.groovy

--- testing-karavan/read_tg.groovy
+++ testing-karavan/read_tg.groovy
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 def test = body
 List<String> arrText = new ArrayList<>()
-def outputString = "List of last sent messages is: "
+def outputString = "List of last sent messages in TG is: "
 
 test.result?.each { item -> 
     arrText.add(item.message?.text as String)
