c6dd13fe82e7669e5466482e614608880ff68456
developer <developer@platform.platform>
2026-01-29 15:40:20 +0000

    29/01/2026, 16:40:18

--- testing-karavan/read_tg.groovy
+++ testing-karavan/read_tg.groovy
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 def test = body
 List<String> arrText = new ArrayList<>()
-def outputString = "List of last sent messages in TG is: "
+def outputString = "List of last sent messages is: "
 
 test.result?.each { item -> 
     arrText.add(item.message?.text as String)
